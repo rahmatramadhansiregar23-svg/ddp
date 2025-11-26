f51c5288cfe931f4b55ea8e5ecd5c6ae7aebf145
rahmatramadhansiregar23-svg <rahmatramadhansiregar23@gmail.com>
2025-11-26 11:09:34 +0700

    Add files via upload

---
+++ DDP/bangunDatar.py
@@ -0,0 +1,9 @@
+import bangunDatar
+
+
+print(" BANGUN DATAR ~~")
+print(f"Luas Persegi adalah ")
+print(f"Luas Persegi Panjang adalah ")
+print(f"Luas Segitiga adalah ")
+print(f"Luas Lingkaran adalah ")
+print(f"Luas JajarGanjar adalah ")
